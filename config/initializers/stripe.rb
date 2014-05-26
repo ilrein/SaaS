47e7ddc65b8a4d29d167cbcb0d764dd6a7049e10
Ilia Reingold <ilia.reingold@gmail.com>
2014-05-26 13:08:14 -0400

    alright i think i got payments to work, now subscription time

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,3 +4,6 @@ Rails.configuration.stripe = {
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+# Stripe.api_key = "sk_test_iV3Sehqbl4s7pRgwhvKbRUR6"
+# STRIPE_PUBLIC_KEY = "pk_test_MY35hCMImMXvM75jBvphmJug"
